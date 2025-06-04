6e88fe290d03721e612deb96b47abd12e22d3e9f
ram-chadar <javaprojectwork2715@gmail.com>
2025-06-04 14:34:51 +0530

    first commit

---
+++ src/main/java/com/tka/Operation.java
@@ -0,0 +1,67 @@
+package com.tka;
+
+public class Operation {
+	
+	public static void pallindrome() {
+		
+	}
+	
+	public static void factorial() {
+		
+	}
+	
+	public static void prime() {
+
+	}
+	
+	public static void fibonacci() {
+
+	}		
+	
+	public static void armstrong() {
+
+	}
+	
+	public static void perfect() {
+
+	}
+	
+	
+	public static void sumOfDigits() {
+
+	}
+	
+	public static void reverseString() {
+
+	}
+	
+	public static void reverseNumber() {
+
+	}
+	
+	public static void swapNumbers() {
+
+	}
+	
+	public static void swapStrings() {
+
+	}
+	
+	public static void countVowels() {
+
+	}
+
+	public static void countConsonants() {
+
+	}
+	
+	public static void countWords() {
+
+	}
+	
+	public static void countCharacters() {
+
+	}
+	
+
+}
